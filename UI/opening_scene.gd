3e4423505ea4c3b88b467abdca864e7e5b4dc552
Gabriel <gabrielhaubenhofer@gmail.com>
2025-03-15 10:18:45 +0100

    start_sounddesign

--- UI/opening_scene.gd
+++ UI/opening_scene.gd
@@ -3,10 +3,20 @@ extends Node2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var texture_rect: TextureRect = $CanvasLayer/TextureRect
 @onready var label: Label = $CanvasLayer/MarginContainer/Label
+@onready var audio_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var audio_player2: AudioStreamPlayer = $AudioStreamPlayer2
+@onready var music_bus_id = AudioServer.get_bus_index("Music")
+@onready var sfx_bus_id = AudioServer.get_bus_index("SFX")
+
 var started = false
 func _input(event: InputEvent) -> void:
 	if (event is InputEventKey or event is InputEventMouseButton)and not started:
 		started = true
+		
+		AudioServer.set_bus_effect_enabled(music_bus_id, 1, false)
+		audio_player.volume_db = linear_to_db(0.5)
+		audio_player2.play()
+		#AudioServer.set_bus_effect_enabled(bus_index, 0, true)
 		animation_player.speed_scale = 0.1
 		animation_player.play('Starting_Zoom')
 		label.hide()
@@ -28,6 +38,7 @@ func _ready() -> void:
 	$CanvasLayer/TextureRect/Control/JamCred.modulate.a = 0
 	$CanvasLayer/TextureRect/Control/Jam.modulate.a = 0
 	$CanvasLayer/TextureRect/Control/OurCred.modulate.a = 0
+	audio_player.volume_db = linear_to_db(0.25)
 	animation_player.speed_scale = 1
 	animation_player.play('flicker')
 	
