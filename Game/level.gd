ce36e145f007096e36b3456779ef541afb053e19
Gabriel <gabrielhaubenhofer@gmail.com>
2025-03-15 23:59:28 +0100

    fix_lvl0

--- Game/level.gd
+++ Game/level.gd
@@ -8,4 +8,7 @@ signal finished_level
 func _process(_delta: float) -> void:
 	if get_tree().get_nodes_in_group('Lights').all(func(x): return x.on):
 		finished_level.emit()
+		print(get_tree().get_nodes_in_group('Lights').size())
+
+		print('finsh')
 		
