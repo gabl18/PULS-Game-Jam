1c47ea6d9ec6b6897191ea1d1433c5725dfba80f
Michael <hirt.michaelelias@gmail.com>
2025-03-16 01:29:13 +0100

    A lot of objects

--- Game/level.gd
+++ Game/level.gd
@@ -6,6 +6,8 @@ signal finished_level
 @export var crack: int
 
 func _process(_delta: float) -> void:
+	
 	if get_tree().get_nodes_in_group('Lights').all(func(x): return x.on):
 		finished_level.emit()
+		print(get_tree().get_nodes_in_group('Lights').size())
 		
